8b522efb3c87573c56ec16ebfb078d83f12dbd8c
lv jian wen <1755008029@qq.com>
2022-10-19 08:38:22 +0800

    3 ver

--- 1.js
+++ 1.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         你好 世界！
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  hello world
+// @author       yyy
 // @match        https://www.bilibili.com/video/BV1Da411Z7s7/?spm_id_from=333.1007.top_right_bar_window_history.content.click&vd_source=c5eb802a04858b2fc448030fd2de0484
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
 // @grant        none
@@ -11,6 +11,8 @@
 
 (function() {
     'use strict';
-
+    var a=1;
+    var b=1;
     console.log("hello world");
+    alert(c=a+b);
 })();
